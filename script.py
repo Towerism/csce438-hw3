fc8166cb4ff8669a207c58f4627bd5b7424d0afe
Afisher21 <crazyfisher21@gmail.com>
2017-03-07 14:21:35 -0600

    Code for launching server

--- script.py
+++ script.py
@@ -2,11 +2,15 @@
 # Run the client with one follower, figure out maximum RPC calls
 import subprocess
 
+def launchServer():
+	proc = subprocess.Popen(["./fbsd", "-p", "12323"], stderr = subprocess.PIPE, stdin = subprocess.PIPE, stdout = subprocess.PIPE)
+	return proc
+
 def main():
-				print("In main function");
-				proc = subprocess.Popen(["ls", "-l"], stderr = subprocess.PIPE, stdin = subprocess.PIPE, stdout = subprocess.PIPE)
-				output = proc.communicate()[0]
-				print(output)
+	server = launchServer()
+	proc = subprocess.Popen(["ls", "-l"], stderr = subprocess.PIPE, stdin = subprocess.PIPE, stdout = subprocess.PIPE)
+	output = proc.communicate()[0]
+	print(output)
 				
 				
 
