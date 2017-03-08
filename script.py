c49159c3c696f255aede07738ce8e564cc7c2aab
Afisher21 <crazyfisher21@gmail.com>
2017-03-07 21:23:51 -0600

    Stupid deadlocking python script

--- script.py
+++ script.py
@@ -13,31 +13,45 @@ def launchUser(username):
 	return proc
 
 def main():
+	subprocess.Popen(["rm", "-rf", " *.txt"])
 	server = launchServer()
 	time.sleep(.5) # Give the server time to initialize before client tries to connect
 	# Initialize user and follower
-	testClient = "talker"
+	testClient = "user"	
 	user = launchUser(testClient)
+	clientResponse = user.stdout.readline() # Login successful
+#	print(clientResponse)
+	clientResponse = user.stdout.readline() # Enter commands:
+#	print(clientResponse)
 	user.stdin.write("CHAT\n")
-	print(user.stdout.readline())
 	user.stdin.write("Initialization message\n")
-	print(user.stdout.readline())
 	user.kill()
 	# initialize follower
-	testClient = "follower"
-	follower = launchUser(testClient)
+	testFriend = "friend"
+	follower = launchUser(testFriend)
+	#print(testFriend)
+	friendResponse = follower.stdout.readline() # Welcome back new User
+	friendResponse = follower.stdout.readline() # Enter commands: 
 	follower.stdin.write("JOIN " + testClient + "\n")
-	print(follower.stdout.readline())
-	follower.stdin.write("CHAT\n")
-	print(follower.stdout.readline())
+	friendResponse = follower.stdout.readline() # Succesfully joined 
+	print(friendResponse)
+	follower.stdin.write("CHAT\n") 
+	friendResponse = follower.stdout.readline() # Enter messages to chat
+	#friendResponse = follower.stdout.readline()
+	#friendResponse = follower.stdout.readline()
 	# Test the limits of the system
 	user = launchUser(testClient)
+	clientResponse = user.stdout.readline() # Welcome back User!
+	clientResponse = user.stdout.readline() # Enter commands:
 	user.stdin.write("CHAT\n")
+	clientResponse = user.stdout.readline() # Enter messages to chat 
 	message = " This is a test message. Do you copy?\n"
+	print("Talker last message: ", clientResponse )
+	print("Follower last message: ", friendResponse)
 	for i in range(1,500):
 		user.stdin.write(message)
 		print("Sent: " + message)
-		time.sleep(1/i)
+		time.sleep(5/i)
 		response = follower.stdout.readline()
 		print("Received: " + response)
 		if response is not message:
